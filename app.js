ceb9e2fbeb7b8b146233793ce90b9846eadbddb9
José Luis <Jos Luis>
2017-01-23 23:34:42 -0500

    style

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static( path.join(__dirname, '/bower_components')));
+app.use('/public',  express.static( path.join(__dirname, '/public')));
 
 app.use('/', index);
 app.use('/users', users);
