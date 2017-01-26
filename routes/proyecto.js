13370375efc78fd6dfa517bd9c22c582f4e0739c
lilibethbm <lilibeth.karen@gmail.com>
2017-01-26 11:26:29 -0500

    al fin lee de la base

--- routes/proyecto.js
+++ routes/proyecto.js
@@ -1,16 +1,15 @@
+require('../models/proyecto')
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+//var proyecto= mongoose.model('proyecto');
 
 
-mongoose.createConnection('mongodb://user:1234@ds131139.mlab.com:31139/projects');
-var db=mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
+
 router.get('/', function(req, res) {
     var collection = mongoose.model('proyecto');
     collection.find({}, function(err, proyecto){
         if (err) throw err;
-      	res.json(proyecto);
     });
 });
 
